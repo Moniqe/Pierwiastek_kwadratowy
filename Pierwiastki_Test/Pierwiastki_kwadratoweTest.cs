fdbb22da669745347bc6b37fed72e1e8a60b9f82
Monika Widurska <monikawidurska@op.pl>
2023-06-15 16:28:32 +0200

    Dodanie testów THEORY

---
+++ Pierwiastki_Test/Pierwiastki_kwadratoweTest.cs
@@ -0,0 +1,25 @@
+﻿using Pierwiastek_kwadratowy;
+using System;
+using Xunit;
+
+namespace Pierwiastek_kwadratowy.Test
+{
+    public class Równanie_kwadratoweTest
+    {
+        [Theory]
+        [InlineData(2, 8, -10, 144)]
+        [InlineData(3, 1, 6, -71)]
+        [InlineData(1, 1, 1, -3)]
+        [InlineData(1, 0, 0, 0)]
+
+        public void calculateDelta_calcDelta_getTheValues(int a, int b, int c, double expect)
+        {
+            Równanie_kwadratowe qe = new Równanie_kwadratowe();
+            double result = qe.calcDelta(a, b, c);
+            Assert.Equal(expect, result);
+
+        }
+
+        
+    }
+}
